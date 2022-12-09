49c84811d0d2895d9d77a821d417c3c879f42591
hnz server <hnz@hnz.hnz>
2022-12-09 21:36:34 +0800

    fix autorestart

--- Server_autorestart.ps1
+++ Server_autorestart.ps1
@@ -1,37 +1,70 @@
 # made by Dori
+Import-Module "C:\loki\LogToLoki.psm1" -Force
 
 $startCount = 0
 $crashCount = 0
 $servername = "Torch-Server"
+$lokiJob = "torch_gaalsien"
+$expectedRestartMinutes = 6 * 60
+$host.UI.RawUI.WindowTitle = "SE Auto-Restart: $servername"
+
+Write-LokiLogMessage -Level INFO -Message "SE Auto-Restart Script Started" -Labels @{job=$lokiJob; component="AutoRestart-PowerShell-Script"}
 while ($true)
 {
     $startCount++
-	Write-Output "$(Get-Date -Format o): Starting/Monitoring ($($servername))... Start count: $($startCount)"
-
     $processpath = $(Resolve-Path "Torch.Server.exe")
-
-    if (Get-Process Torch.Server | Where-Object {$_.Path -like $processpath})
+    $process = Get-Process Torch.Server | Where-Object {($_.Path -like $processpath) -and (-not $_.HasExited)} | Select-Object -First 1
+    if (!!$process)
     {
-        $process = Get-Process Torch.Server | Where-Object {$_.Path -like $processpath}
+        Write-LokiLogMessage -Level INFO -Message "Monitoring ($($servername))... Start count: $($startCount)" -Labels @{job=$lokiJob; component="AutoRestart-PowerShell-Script"}
+        #Write-Output "$(Get-Date -Format o): Monitoring ($($servername))... Start count: $($startCount)"
     }
     else
     {
+        Write-LokiLogMessage -Level INFO -Message "Starting ($($servername))... Start count: $($startCount)" -Labels @{job=$lokiJob; component="AutoRestart-PowerShell-Script"}
+        #Write-Output "$(Get-Date -Format o): Starting ($($servername))... Start count: $($startCount)"
         $process = Start-Process $(Resolve-Path "Torch.Server.exe") -PassThru
     }
-	
+	$handle = $process.Handle
 	$crashFilePath = "$processpath\$($process.Id).$($servername)"
+    #Write-Output "$(Get-Date -Format o): Watching process with PID $($process.Id)"
+    $startTime = $process.StartTime
+    Write-LokiLogMessage -Level INFO -Message "Watching process with PID $($process.Id) started at ${startTime}" -Labels @{job=$lokiJob; component="AutoRestart-PowerShell-Script"}
 
-	while (!$process.HasExited)
+    $process.WaitForExit()
+	<#while (!$process.HasExited)
 	{
 		if ($(Get-Item $crashFilePath -ErrorAction SilentlyContinue) -ne $null)
 		{
 			$crashCount++
-			$Host.UI.WriteErrorLine("$(Get-Date -Format o): $($servername) crashed/restarted... Restarting/Monitoring server. Crash/Restart count: $($crashCount)")
+            Write-LokiLogMessage -Level ERROR -Message "$($servername) crashed/restarted... Restarting/Monitoring server. Crash/Restart count: $($crashCount)" -Labels @{job=$lokiJob; component="AutoRestart-PowerShell-Script"}
+			#$Host.UI.WriteErrorLine("$(Get-Date -Format o): $($servername) crashed/restarted... Restarting/Monitoring server. Crash/Restart count: $($crashCount)")
 			Stop-Process -InputObject $process
 			Remove-Item $crashFilePath
 			Start-Sleep 2
 			break
 		}
 		Start-Sleep 2
-	}
+	}#>
+    $timeRan = ((Get-Date) - $startTime)
+    #Write-Output "$(Get-Date -Format o): PID $($process.Id) exited with code $($process.ExitCode)"
+    if ($process.ExitCode -eq $null) {
+        if ($timeRan.TotalMinutes -lt $expectedRestartMinutes) { 
+            Write-LokiLogMessage -Level WARN -Message "$($servername) potentially crashed. Time Ran: ${timeRan}" -Labels @{job=$lokiJob; component="AutoRestart-PowerShell-Script"}
+        } else {
+            Write-LokiLogMessage -Level INFO -Message "$($servername) exited. Time Ran: ${timeRan}" -Labels @{job=$lokiJob; component="AutoRestart-PowerShell-Script"}
+        }
+    } else {
+        if ($process.ExitCode -eq 0) {
+            Write-LokiLogMessage -Level INFO -Message "$($servername) exited with ExitCode $($process.ExitCode). Time Ran: ${timeRan}" -Labels @{job=$lokiJob; component="AutoRestart-PowerShell-Script"}
+        } else {
+            if ($timeRan.TotalMinutes -lt $expectedRestartMinutes) { 
+                Write-LokiLogMessage -Level WARN -Message "$($servername) potentially crashed with ExitCode $($process.ExitCode). Time Ran: ${timeRan}" -Labels @{job=$lokiJob; component="AutoRestart-PowerShell-Script"}
+            } else {
+                Write-LokiLogMessage -Level INFO -Message "$($servername) exited with ExitCode $($process.ExitCode). Time Ran: ${timeRan}" -Labels @{job=$lokiJob; component="AutoRestart-PowerShell-Script"}
+            }
+        }
+    } 
+    Remove-Variable process
+    Remove-Variable handle
 }
